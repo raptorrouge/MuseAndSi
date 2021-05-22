a2ca569d130e4d992fa9ab2f6b840e299e685d41
raptorrouge <raptorrouge@gmail.com>
2021-05-22 13:25:03 +0200

    ajout bdd + pdo

---
+++ site final/pdo.php
@@ -0,0 +1,26 @@
+<?php
+
+define("database", "museandsi");
+define("hostname", "localhost");
+define("username", "root");
+define("password", "");
+
+
+$dsn = 'mysql:dbname=' . database . ';host=' . hostname . ';charset=utf8';
+
+$bdd = new PDO($dsn, username, password);
+// pour afficher les erreurs
+$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// pour récupérer le résultat des requêtes SELECT sous forme de tableaux associatifs
+$bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$sql = " SELECT * FROM user WHERE id_user = ?";
+$stmt = $bdd->prepare($sql);
+$stmt->execute([1]);
+
+var_dump($stmt->fetch());
+while ($row = $stmt->fetch()) {
+    echo $row;
+}
+
+
