a1175750bbabd1b1134b630a74c75657b9d2b75b
raptorrouge <raptorrouge@gmail.com>
2021-05-30 18:57:01 +0200

    inscription ok PHP

--- site final/inscirption.php
+++ site final/inscirption.php
@@ -4,28 +4,45 @@
 <?php
 include("pdo.php");
 $erreurMail=false;
+$erreurMail2=false;
 $erreurNom=false;
 $erreurPrenom=false;
 $erreurAge=false;
-$erreurAdresse=false;
 $erreurRole=false;
-echo "1";
 
-if ( isset($_POST["mail"]) and isset($_POST["mdp"]) and isset($_POST["nom"]) and isset($_POST["prenom"]) and isset($_POST["adresse"]))
+
+
+if ( isset($_POST["mail"]) and isset($_POST["mdp"]) and isset($_POST["nom"]) and isset($_POST["prenom"]) and isset($_POST["age"]))
 {
-    echo "2";
 
     $donnees["mail"]=htmlspecialchars($_POST["mail"]);
     $donnees["mdp"]=hash('sha256',$_POST["mdp"]);
     $donnees["nom"]=htmlspecialchars($_POST["nom"]);
     $donnees["prenom"]=htmlspecialchars($_POST["prenom"]);
     $donnees["age"]=htmlspecialchars($_POST["age"]);
-    $donnees["role"]=htmlspecialchars($_POST["adresse"]);
-    $donnees["adresse"]=htmlspecialchars($_POST["adresse"]);
+    $donnees["role"]=htmlspecialchars($_POST["role"]);
+
 
     if (!filter_var($donnees["mail"], FILTER_VALIDATE_EMAIL)) {
         $erreurMail = true;
+    }else{
+
+        $requeteMail= ("select EMAIL from user;");
+        $dataMails = $bdd2->prepare($requeteMail);
+        $dataMails->execute();
+        while ($row = $dataMails->fetch(PDO::FETCH_ASSOC)) {
+
+            foreach($row as $mail){
+                    if( $donnees["mail"]==$mail){
+
+                        $erreurMail2 = true;
+                        break;
+                    }
+                }
+        }
     }
+
+
 
     if (!preg_match("/[A-Za-z]{2,}/", $donnees["nom"])) {
         $erreurNom = true;
@@ -33,29 +50,22 @@ if ( isset($_POST["mail"]) and isset($_POST["mdp"]) and isset($_POST["nom"]) and
     if (!preg_match("/[A-Za-z]{2,}/", $donnees["prenom"])) {
         $erreurPrenom = true;
     }
-    if (!preg_match("/(2[0-1]|1[0-9]|0[0-9])/", $donnees["age"])) {
+    if ( (int)$donnees["age"]<10 || (int)$donnees["age"]>199) {
         $erreurAge = true;
     }
-    if (!preg_match("/[A-Za-z]{2,}/", $donnees["adresse"])) {
-        $erreurAdresse = true;
-    }
 
-    if (sizeof($_POST["role"])!=1) {
-        $erreurRole = true;
-    } else{
-        foreach ($_POST["role"] as  $role ){
-            $donnees["role"]=htmlspecialchars($role);
-            echo $role;
-        }
+
+    if($_POST["role"]==""){
+        $erreurRole=true;
     }
 
 
-    if (!$erreurAdresse and !$erreurNom and !$erreurPrenom and !$erreurAge) {
+
+
+    if (!$erreurMail and !$erreurMail2  and !$erreurRole  and !$erreurNom and !$erreurPrenom and !$erreurAge) {
         $ma_requete = ("INSERT INTO user(EMAIL,MDP,NOM_User,PRENOM_User,AGE_User,ROLE_User) VALUES('". $donnees["mail"] . "','". $donnees["mdp"] . "','". $donnees["nom"] . "' , '" .  $donnees["prenom"]. "','" . $donnees["age"] . "','" . $donnees["role"] . "');"  );
         echo $ma_requete;
         $bdd->exec($ma_requete);
-        echo "3";
-
     }
 
 
@@ -71,9 +81,10 @@ if ( isset($_POST["mail"]) and isset($_POST["mdp"]) and isset($_POST["nom"]) and
             <label>Email
                 <input name="mail" type="text" size=18 value="<?php if(isset($donnees["mail"])){echo ($donnees["mail"]);}?>"/>
                 <?php if ($erreurMail){echo ("<div style='color: red'>Veuillez rentrer une adresse mail valide</div>");} else {echo ("<br>");}?>
+                <?php if ($erreurMail2){echo ("<div style='color: red'>Mail deja utilisé</div>");} else {echo ("<br>");}?>
             </label>
-            <label>Mot de passe
-                <input name="mdp" type="text" size=18 value="<?php if(isset($_POST["mdp"])){echo ($_POST["mdp"]);}?>"/>
+            <label>Mot de passe de 8 carractère minimum
+                <input name="mdp" type="password" minlength="8" required value="<?php if(isset($_POST["mdp"])){echo ($_POST["mdp"]);}?>"/>
                 <?php echo ("<br>");?>
             </label>
             <label>Nom
@@ -86,23 +97,17 @@ if ( isset($_POST["mail"]) and isset($_POST["mdp"]) and isset($_POST["nom"]) and
             </label>
             <label>Age
                     <input name="age" type="text" size=18 value="<?php if(isset($donnees["age"])){echo ($donnees["age"]);}?>"/>
-                <?php if ($erreurAge){echo ("<div style='color: #ff0000'>Veuillez rentrer un age valide entre 0 et 199 ans</div>");} else {echo ("<br>");}?>
-            </label>
-            <label>Ville d'habitation
-                <input name="adresse" type="text" size=18 value="<?php if(isset($donnees["adresse"])){echo ($donnees["adresse"]);}?>"/>
-                <?php if ($erreurAdresse){echo ("<div style='color: red'>Veuillez rentrer une ville de plus de deux lettres</div>");} else {echo ("<br>");}?>
+                <?php if ($erreurAge){echo ("<div style='color: #ff0000'>Veuillez rentrer un age valide entre 10 et 199 ans</div>");} else {echo ("<br>");}?>
             </label>
             <label> Choisiser votre role
                 <br>
-                <?php
-                $roles = array("Musicien","Auditeur");
-                for($i=0;$i<sizeof($roles);$i++){
-                    echo"<input type='checkbox' name='role[]' value='".$roles[$i]."'/>".$roles[$i]."<br>";
-                }
-                ?>
-                <?php if ($erreurRole){echo ("<div style='color: red'>Veuillez choisir un seul role</div>");} else {echo ("<br>");}?>
-
+                <select name="role" id="role">
+                    <option value="Auditeur">Auditeur</option>
+                    <option value="Musicien">Musicien</option>
+                    <?php echo $_POST["role"]; if ($erreurRole){echo ("<div style='color: #ff0000'>Choisisez un role</div>");} else {echo ("<br>");}?>
+                </select>
             </label>
+            <br>
             <button type="submit">Valider</button>
         </fieldset>
     </div>
--- site final/pdo.php
+++ site final/pdo.php
@@ -17,16 +17,17 @@ $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 // pour récupérer le résultat des requêtes SELECT sous forme de tableaux associatifs
 $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-/*$sql = " SELECT * FROM user";
+$bdd2 = new PDO($dsn, username, password);
+// pour afficher les erreurs
+$bdd2->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// pour récupérer le résultat des requêtes SELECT sous forme de tableaux associatifs
+$bdd2->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$sql = " SELECT EMAIL FROM user";
 $stmt = $bdd->prepare($sql);
-$stmt->execute([1]);*/
+$stmt->execute([1]);
 
 
 
-/*var_dump($stmt->fetch());
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $user = new user($row);
-    var_dump($user);
-}*/
 
 
